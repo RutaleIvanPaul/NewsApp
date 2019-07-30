3079aa437628d8f5846def2be26efe4133117e0a
RutaleIvanPaul <paulivan568@gmail.com>
2019-07-30 11:12:44 +0300

    Add news details Activity

--- app/src/main/java/com/kotlin/ivanpaulrutale/newsapp/CustomAdapter.kt
+++ app/src/main/java/com/kotlin/ivanpaulrutale/newsapp/CustomAdapter.kt
@@ -1,14 +1,17 @@
 package com.kotlin.ivanpaulrutale.newsapp
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
-class CustomAdapter(val newsList: ArrayList<News>): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+class CustomAdapter
+    (val newsList: ArrayList<News>, val context: Context): RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        val v = LayoutInflater.from(p0?.context).inflate(R.layout.list_item,p0,false)
+        val v = LayoutInflater.from(p0.context).inflate(R.layout.list_item,p0,false)
         return ViewHolder(v)
     }
 
@@ -16,9 +19,15 @@ class CustomAdapter(val newsList: ArrayList<News>): RecyclerView.Adapter<CustomA
         return newsList.size
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        val news:News = newsList[p1]
-        p0?.textView.text = news.headline
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val news:News = newsList[position]
+        holder?.textView.text = news.headline
+
+        holder.itemView.setOnClickListener{view ->
+            val intent = Intent(view.context, NewsDetails::class.java)
+            intent.putExtra("news_headline", news.headline)
+            context.startActivity(intent)
+        }
     }
 
     class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
---
+++ app/src/main/java/com/kotlin/ivanpaulrutale/newsapp/NewsDetails.kt
@@ -0,0 +1,26 @@
+package com.kotlin.ivanpaulrutale.newsapp
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_news_details.*
+
+class NewsDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_details)
+        setSupportActionBar(toolbar)
+
+        val news_headline = intent.getStringExtra("news_headline")
+        findViewById<TextView>(R.id.headlineTextView).text = news_headline
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
